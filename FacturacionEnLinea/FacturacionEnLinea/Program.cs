3d734a485e4c5654570d7894eb04bfc805017552
FavioLopez <fab00442@gmail.com>
2020-10-24 11:49:59 -0400

    Creando el proyecto, agregando la libreria de algoritmos y agregando las pruebas unitarias

---
+++ FacturacionEnLinea/FacturacionEnLinea/Program.cs
@@ -0,0 +1,16 @@
+﻿using Algoritmos;
+using System;
+
+namespace FacturacionEnLinea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodigoControl codigoControl = new CodigoControl();
+
+            Console.WriteLine(codigoControl.GenerarCodigoControl("79040011859", "152", "1026469026", "20070728", "135", "A3Fs4s$)2cvD(eY667A5C4A2rsdf53kw9654E2B23s24df35F5"));
+
+        }
+    }
+}
